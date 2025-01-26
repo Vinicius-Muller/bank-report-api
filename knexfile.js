28c94336c8d4cfbeebee6a706cc2674df3abb0ce
vinicius <viniciusmul98@gmail.com>
2025-01-26 13:04:00 -0300

    migration init

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  staging: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./src/db/migrations",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./src/db/migrations",
+    },
+  },
+};
